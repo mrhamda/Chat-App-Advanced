bc3370d2d6371a77898280af48183dafe450166a
Abdullah Hamdan <98427207+mrhamda@users.noreply.github.com>
2026-01-09 12:36:10 +0100

    Update route.ts

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -2,33 +2,28 @@ import { NextRequest, NextResponse } from "next/server";
 import path from "path";
 import fs from "fs";
 
-// Set the upload directory
 const UPLOAD_DIR = path.resolve(process.cwd(), "public/uploads");
 
-// Utility function to generate a unique name
 const generateUniqueName = (originalName: string) => {
-  const timestamp = Date.now(); // Current timestamp
-  const extension = path.extname(originalName); // Extract the file extension
-  return `file_${timestamp}${extension}`; // Combine timestamp and extension
+  const timestamp = Date.now(); 
+  const extension = path.extname(originalName); 
+  return `file_${timestamp}${extension}`; 
 };
 
 export const POST = async (req: NextRequest) => {
   try {
-    // Parse the form data to get the file
     const formData = await req.formData();
-    const file = formData.get("file"); // Get the file directly from formData
+    const file = formData.get("file"); 
 
     if (file && file instanceof Blob) {
-      // Convert the file to a Buffer for saving
       const buffer = Buffer.from(await file.arrayBuffer());
 
-      // Ensure the directory exists, if not, create it
       if (!fs.existsSync(UPLOAD_DIR)) {
         fs.mkdirSync(UPLOAD_DIR, { recursive: true });
       }
 
-      const originalName = (file as File).name; // Get the original file name
-      const generatedName = generateUniqueName(originalName); // Generate a unique name
+      const originalName = (file as File).name; 
+      const generatedName = generateUniqueName(originalName); 
       const filePath = path.resolve(UPLOAD_DIR, generatedName);
 
       fs.writeFileSync(filePath, buffer);
