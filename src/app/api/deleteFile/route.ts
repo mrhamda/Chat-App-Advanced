67227e1de8b9dd96e500e766e88881706e263a75
Abdullah Hamdan <98427207+mrhamda@users.noreply.github.com>
2026-01-09 12:35:21 +0100

    Update route.ts

--- src/app/api/deleteFile/route.ts
+++ src/app/api/deleteFile/route.ts
@@ -10,23 +10,18 @@ export const POST = async (req: NextRequest) => {
       return NextResponse.json({ error: 'Message ID and text are required' }, { status: 400 });
     }
 
-    // Assuming the txt field contains the relative path to the file
     const filePath = path.join(process.cwd(), 'public', txt);
 
     try {
-      // Check if the file exists
-      await fs.stat(filePath);  // This checks if the file exists
+      await fs.stat(filePath); 
 
-      // Delete the file
       await fs.unlink(filePath);
 
       console.log(`Deleted file: ${filePath}`);
     } catch (err) {
-      // If the file doesn't exist, we can just log it
       console.log(`File not found: ${filePath}`);
     }
 
-    // Optionally, handle updating the message status or deleting the message from the database
     return NextResponse.json({ success: true });
   } catch (error) {
     console.error(error);
